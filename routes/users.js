da0388958cd65fc050197c53a00ef42cb2bf9219
velansalis <velanmax8@gmail.com>
2018-05-18 17:03:30 +0530

    Added Patch and Put routes for users

--- routes/users.js
+++ routes/users.js
@@ -9,5 +9,7 @@ router.post("/login", UsersCtrl.loginUser);
 router.get("/", UsersCtrl.getAllUsers);
 router.get("/:username", UsersCtrl.getSingleUser);
 router.delete("/:username", UsersCtrl.deleteUser);
+router.patch("/:username", UsersCtrl.updateUser);
+router.put("/:username", UsersCtrl.updateUserMany);
 
 module.exports = router;
