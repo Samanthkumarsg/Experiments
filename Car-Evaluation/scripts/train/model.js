1b32b7a4dee484cc36c6a4c0289fa94f51ff161d
velansalis <velanmax8@gmail.com>
2018-07-31 20:10:53 +0530

    Defined the model

--- Car-Evaluation/scripts/train/model.js
+++ Car-Evaluation/scripts/train/model.js
@@ -1,2 +1,9 @@
 const tf = require("@tensorflow/tfjs");
 require("@tensorflow/tfjs-node");
+
+const model = tf.sequential();
+
+module.exports = {
+	model: model,
+	tf: tf
+};
--- Car-Evaluation/scripts/train/train.js
+++ Car-Evaluation/scripts/train/train.js
@@ -1,15 +1,27 @@
+const model = require("./model").model;
+const tf = require("./model").tf;
 const convertedData = require("./data");
 const data = convertedData.data;
 const labels = convertedData.labels;
 
 const batchSize = 3;
+const iterations = 1;
 
 function train() {
 	tf.tidy(() => {
 		for (i = 0; i < iterations; i++) {
-			let batch = getBatch(data.xs, i, batchSize);
+			let batch = getBatch(data, i, batchSize);
 		}
 	});
 }
 
-function getBatch(array, start, size) {}
+function getBatch(array, i, size) {
+	let _xs = array.xs;
+	let _ys = array.ys;
+	let start = i * size;
+	let end = start + size;
+	return {
+		xs: _xs.slice(start, end),
+		ys: _ys.slice(start, end)
+	};
+}
