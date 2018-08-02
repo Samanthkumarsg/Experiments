5a2ab2e217e94e13cbc99dd5372df9f0d12c6555
velansalis <velanmax8@gmail.com>
2018-08-02 17:56:48 +0530

    UI completed

--- Color-Classifier/src/dist/index.js
+++ Color-Classifier/src/dist/index.js
@@ -1,6 +1,31 @@
 colorCollection = {};
 let canvas = document.querySelector("canvas");
+let context = canvas.getContext("2d");
 canvas.height = 400;
 canvas.width = 350;
 
-function startPredictions() {}
+let presentColor;
+let container = [];
+
+function putColor(color) {
+	let r = Math.floor(Math.random() * 255);
+	let g = Math.floor(Math.random() * 255);
+	let b = Math.floor(Math.random() * 255);
+	presentColor = `rgb(${r},${g},${b})`;
+	container.push({
+		color: [r, g, b],
+		label: color
+	});
+	context.fillStyle = presentColor;
+	context.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function addColor(obj) {
+	let selectedColor = obj.classList[1];
+	console.log(presentColor + " - " + selectedColor);
+	putColor(selectedColor);
+}
+
+function train() {
+	console.log(container);
+}
