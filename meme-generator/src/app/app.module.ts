41a0ce4e2da3077073d1eb0b2f732194fdda6db0
velansalis <velanmax8@gmail.com>
2018-08-16 15:47:21 +0530

    Added navigation bar

--- meme-generator/src/app/app.module.ts
+++ meme-generator/src/app/app.module.ts
@@ -1,16 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./components/root/app.component";
+import { NavigationComponent } from "./components/navigation/navigation.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, NavigationComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
