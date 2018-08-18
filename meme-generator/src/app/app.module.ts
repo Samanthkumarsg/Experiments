e6e5ba5ae8b7afd3e34532f40ba4d9ac1e8f51f1
velansalis <velanmax8@gmail.com>
2018-08-18 11:37:19 +0530

    Removed navigation

--- meme-generator/src/app/app.module.ts
+++ meme-generator/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./components/root/app.component";
 import { NavigationComponent } from "./components/navigation/navigation.component";
+import { SearchComponent } from "./components/search/search.component";
 
 @NgModule({
-  declarations: [AppComponent, NavigationComponent],
+  declarations: [AppComponent, NavigationComponent, SearchComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
