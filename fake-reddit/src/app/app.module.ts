c213bdbd353d7e37febb28cd488302e1cab8e454
velansalis <velanmax8@gmail.com>
2018-08-20 11:08:48 +0530

    Added search block

--- fake-reddit/src/app/app.module.ts
+++ fake-reddit/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./root/app.component";
-import { NavComponent } from './components/nav/nav.component';
+import { NavComponent } from "./components/nav/nav.component";
+import { ContentComponent } from "./components/content/content.component";
 
 @NgModule({
-  declarations: [AppComponent, NavComponent],
+  declarations: [AppComponent, NavComponent, ContentComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
