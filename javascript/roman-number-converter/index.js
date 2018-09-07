0ecb40c953777c7ff5d4b0ef001e797f64945846
velansalis <velanmax8@gmail.com>
2018-09-07 21:19:57 +0530

    Done with roman

--- javascript/roman-number-converter/index.js
+++ javascript/roman-number-converter/index.js
@@ -1,25 +1,31 @@
 // Convert the given number into a roman numeral.
 // All roman numerals answers should be provided in upper-case.
 // Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+var index = {
+	M: 1000,
+	CM: 900,
+	D: 500,
+	CD: 400,
+	C: 100,
+	XC: 90,
+	L: 50,
+	XL: 40,
+	X: 10,
+	IX: 9,
+	V: 5,
+	IV: 4,
+	I: 1
+};
 
 function convertToRoman(num) {
-	let number = num;
-	let roman = new Array();
-	if (parseInt(num) == NaN) {
-		console.log("Invalid input");
-		return;
-	} else {
-		let digit,
-			index = 1;
-		while (parseInt(num) > 0) {
-			digit = num % 10;
-			index *= 10;
-			num /= 10;
-			console.log(parseInt(digit));
+	let roman = "";
+	for (let i in index) {
+		while (num >= index[i]) {
+			roman += i;
+			num -= index[i];
 		}
 	}
+	return roman;
 }
 
-function returnRoman() {}
-
-convertToRoman(12);
+console.log(convertToRoman(200));
