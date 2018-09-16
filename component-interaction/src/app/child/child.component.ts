ba3e5435811ec3deac017f16ef8684ede0a781c2
velansalis <velanmax8@gmail.com>
2018-09-16 16:34:37 +0530

    Done

--- component-interaction/src/app/child/child.component.ts
+++ component-interaction/src/app/child/child.component.ts
@@ -1,11 +1,21 @@
-import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  ViewChild
+} from "@angular/core";
+import { SiblingComponent } from "../sibling/sibling.component";
+
+import { CommonService } from "../common.service";
 @Component({
   selector: "app-child",
   templateUrl: "./child.component.html",
   styleUrls: ["./child.component.css"]
 })
 export class ChildComponent implements OnInit {
-  constructor() {}
+  constructor(public cs: CommonService) {}
 
   @Input("anotherText")
   childText: string = "**none**";
@@ -13,7 +23,15 @@ export class ChildComponent implements OnInit {
   @Output()
   changeValue = new EventEmitter();
 
+  @ViewChild(SiblingComponent)
+  sibcomp: SiblingComponent;
+
+  changeSiblingValue() {
+    this.cs.siblingValue++;
+  }
+
   ngOnInit() {
+    this.cs.siblingValue = 40;
     this.changeValue.emit("TextFromChild");
   }
 }
--- component-interaction/src/app/sibling/sibling.component.ts
+++ component-interaction/src/app/sibling/sibling.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { CommonService } from "../common.service";
 
 @Component({
-  selector: 'app-sibling',
-  templateUrl: './sibling.component.html',
-  styleUrls: ['./sibling.component.css']
+  selector: "app-sibling",
+  templateUrl: "./sibling.component.html",
+  styleUrls: ["./sibling.component.css"]
 })
 export class SiblingComponent implements OnInit {
+  constructor(public cs: CommonService) {}
 
-  constructor() { }
+  siblingVal: number = 0;
 
   ngOnInit() {
+    this.siblingVal = this.cs.siblingValue;
   }
-
 }
