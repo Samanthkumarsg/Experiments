c4dd042f7c810ee8439769ad0268c66a74bd246c
velansalis <velanmax8@gmail.com>
2018-09-30 22:06:30 +0530

    Basic usage done

--- experiments/async-calls/js/index.js
+++ experiments/async-calls/js/index.js
@@ -1,15 +1,23 @@
+//
 let count = 0;
+let globalVal = new Array();
 
-const event = new rxjs.Observable(observer => {
-  observer.next("Value " + count);
-  ++count;
-  observer.next("Value " + count);
-});
+const Subject = new rxjs.Subject();
 
-console.log(event);
-
-event.subscribe(
-  val => console.log(val),
-  err => console.error(err),
-  complete => console.log(complete)
+Subject.subscribe(
+  data => {
+    globalVal.push(data);
+    console.log(globalVal);
+  },
+  err => {
+    console.error(err);
+  },
+  complete => {
+    console.log("Completed");
+  }
 );
+
+setInterval(() => {
+  Subject.next(count++);
+  if (count > 5) Subject.complete();
+}, 100);
