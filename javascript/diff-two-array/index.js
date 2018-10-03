8414b8654aade3817636a740a7347dd799ba9ce1
velansalis <velanmax8@gmail.com>
2018-10-03 21:12:40 +0530

    Refactored

--- javascript/diff-two-array/index.js
+++
@@ -1,9 +0,0 @@
-function diffArray(arr1, arr2) {
-  var newArr = [];
-  arr1.forEach(element => {
-    if (arr2.indexOf(element)) newArr.push(element);
-  });
-  return newArr;
-}
-
-console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
