e3c6fd6320fabeef2960fb35ff666aba84056bc7
velansalis <velanmax8@gmail.com>
2018-10-07 08:38:08 +0530

    Socket Defined

--- Java/Socket/ClientSocketClass.java
+++ Java/Socket/ClientSocketClass.java
@@ -1,2 +1,26 @@
 import java.io.*;
 import java.net.*;
+class ClientSocketClass{
+    public static void main(String args[]) {
+        
+        // Defining the Streams
+        DataInputStream dis = null;
+        DataOutputStream dos = null;
+        Socket sc = null;
+
+        try {
+            // Defining the Socket
+            sc = new Socket("localhost",9999);
+            dis = new DataInputStream(sc.getInputStream());
+            dos = new DataOutputStream(sc.getOutputStream());
+
+            // Getting stream
+            System.out.println(dis.readUTF());
+            dis.close();
+        } catch (Exception e) {
+            //TODO: handle exception
+            System.out.println(e);
+        }
+
+    }
+}
