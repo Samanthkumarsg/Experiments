18d7c3f9890181b6855cd592edb01c71682dcc4b
velansalis <velanmax8@gmail.com>
2018-10-09 10:51:10 +0530

    Done with basic

--- Chrome-ex/hello-world/background.js
+++ Chrome-ex/hello-world/background.js
@@ -1,1 +1,4 @@
 console.log("Hello from Background");
+
+chrome.runtime.sendMessage("Hey");
+console.log(chrome);
--- Chrome-ex/hello-world/content.js
+++ Chrome-ex/hello-world/content.js
@@ -1,1 +1,4 @@
 console.log("Hello World from Content");
+chrome.runtime.onMessage.addListener(function(val) {
+  console.log(val);
+});
